18873623d870dcf453e7e6cfdb010f9e1f242dc2
Someone <rgbjkbs@hdbf.ru>
2016-10-26 20:19:24 +0300

    start page of site and nb to gitignore list

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Yo-ho-ho</h1>
+        <img src="http://img-fotki.yandex.ru/get/9058/121447594.558/0_f991b_265867d2_XL.jpg" style="width:100%"/>
+    </body>
+</html>
